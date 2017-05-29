5c0f98f5083108e196f218f3812a44a1abc48f9c
Hamid <Hamid.vetr@gmail.com>
2017-05-29 16:51:23 +0430

    added telegram sdk package, set webhook to zhenic

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
+use Telegram\Bot\Laravel\Facades\Telegram;
+
 Route::get('/', function () {
-    return view('welcome');
+//    $response = Telegram::getMe();
+//
+//    $botId = $response->getId();
+//    $firstName = $response->getFirstName();
+//    $username = $response->getUsername();
+
+//    $response = Telegram::getUpdates();
+
+//    $response = Telegram::sendMessage([
+//        'chat_id' => '78182085',
+//        'text' => 'Hello World'
+//    ]);
+//
+//    $messageId = $response->getMessageId();
+
+    $response = Telegram::setWebhook(['url' => 'https://zhenic.co/393184030:AAFwWpAtEWS53XuxOK543FiwJRoJUEI-r5U/webhook']);
+
+    dd($response);
 });
